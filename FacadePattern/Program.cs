01cbc4ea8c7b95bff28b3b6ecced4f38cea6ebf6
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2021-02-28 15:06:02 +0200

    Initial Create

---
+++ FacadePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating the Facade
+
+            Mortgage mortgage = new Mortgage();
+
+            // Evaluate mortgage eligibility for customer
+
+            Customer customer = new Customer("Aya AbdulSlam");
+            bool eligible = mortgage.IsEligible(customer, 1000000);
+
+            Console.WriteLine("\n" + customer.Name +
+                " has been " + (eligible ? "Approved" : "Rejected"));
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
